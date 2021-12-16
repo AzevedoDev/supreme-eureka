7b445978132e531547d058c7824649582abbacf9
Pedro Azevedo <theazevedo@outlook.com>
2021-12-16 08:17:50 -0300

    Remove commments and start ws connect

--- main.js
+++ main.js
@@ -1,126 +1,58 @@
-import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
-import { Language, minifyHTML } from "https://deno.land/x/minifier/mod.ts";
+import { cheerio } from 'https://deno.land/x/cheerio@1.0.4/mod.ts';
 // https://www.npmjs.com/package/cheerio
 
-// const url = "https://www.ygoscope.com/playerProfile?player=youngest+Thief+II";
+const url = 'https://www.ygoscope.com/playerProfile?player=dragshowbaby';
 
-console.log("Start");
-console.time("request");
-// const response = await fetch(url);
+console.log('Start');
+console.time('request');
+const response = await fetch(url);
 // console.log(response);
-console.timeEnd("request");
+console.timeEnd('request');
 
-const code = `
-<html>
-<head>
-  <style>
-    html {
-      margin: 0;
-      padding: 0;
-    }
-  </style>
-  <script>
-    const x = 23;
-    if (x > 20) {
-      x--;
-    }
-    console.log(x);
-  </script>
-</head>
-</html>
-`;
-console.log(typeof code);
-console.time("get html");
-// const htmlText = await response.text();
-const htmlText = await Deno.readTextFileSync("./main2.html");
-console.log(typeof htmlText);
-console.timeEnd("get html");
+console.time('get html');
+const htmlText = await response.text();
+
+// const htmlText = await Deno.readTextFileSync("./main2.html");
+console.timeEnd('get html');
 
 // await Deno.writeTextFile("./main2.html", htmlText);
 
-const texto = await minifyHTML(htmlText.toString());
-console.time("parser");
-const $ = cheerio.load(texto);
-console.timeEnd("parser");
+console.time('parser');
+const $ = cheerio.load(htmlText);
+console.timeEnd('parser');
 
-function TableDataMapper(td) {
-  // ["whybotherthere", "Swordsoul Tenyi" ...]
-  return td.children
-    .filter((c) => c.name === "a")
-    .filter(Boolean)
-    .map((a) => a.children.find((c) => c.type === "text"));
-}
-
-const propValues = $(".table-row-hover")
+const data = $('td')
   .toArray()
-  .map((tr) =>
-    tr.children
-      .filter((node) => node?.type === "tag")
-      .map(TableDataMapper)
-      .flat()
-  )
-  .flat();
+  .map((e) => $(e).text().trim().replace('\n', '').replace(/\s+/g, ' '));
 
-const data = $("td")
+const replays = $('td a')
   .toArray()
-  .map((e) => $(e).text().trim());
+  .map((e) => $(e).attr())
+  .map(({ href }) => href)
+  .filter((e) => e.includes('replay?'));
 
-const replays = propValues
-  .filter((replay) => replay.data === "Replay")
-  .map((c) => c.parent.attribs.href);
+const tableHeader = $('th')
+  .toArray()
+  .map((e) => $(e).text().trim().toLocaleLowerCase())
+  .map((e) => (e === '' ? 'replay' : e))
+  .map((e) => (e === 'player deck' ? 'player_deck' : e))
+  .map((e) => (e === 'opponent deck' ? 'opponent_deck' : e))
+  .filter((e) => e !== 'count' && e !== 'name');
 
-const header = [
-  "Replay",
-  "Opponent",
-  "Player Deck",
-  "Opponent Deck",
-  "Result",
-  "Duration",
-  "Date",
-  "Type",
-  "Log",
-];
+const headersLength = tableHeader.length;
 
-const dataWithoutHeader = data.slice(8);
-
-const headersLength = header.length;
-
-const output = dataWithoutHeader
+const output = data
   .reduce((list, value, index) => {
     const listIndex = Math.floor(index / headersLength);
-    const key = header[index % headersLength];
+    const key = tableHeader[index % headersLength];
 
     list[listIndex] = list[listIndex] || {};
     list[listIndex][key] = value;
-    list[listIndex][key] === "Replay"
-      ? (list[listIndex][key] = replays[listIndex])
-      : null;
+    list[listIndex][key] === 'Replay' &&
+      (list[listIndex][key] = replays[listIndex]);
+
     return list;
   }, [])
   .splice(0, 19);
 
 console.log(output);
-// const example = [
-//   {
-//     Opponent: "whybotherthere",
-//     "Player Deck": "Swordsoul Tenyi",
-//     "Opponent Deck": "Swordsoul Tenyi Yang Zing",
-//     Result: "WON",
-//     Duration: "13:45",
-//     Date: "05-12-2021",
-//     Type: "MATCH",
-//     Log: "34473786",
-//     Replay: "Replay",
-//   },
-//   {
-//     Opponent: "whybotherthere",
-//     "Player Deck": "Swordsoul Tenyi",
-//     "Opponent Deck": "Swordsoul Tenyi Yang Zing",
-//     Result: "WON",
-//     Duration: "13:45",
-//     Date: "05-12-2021",
-//     Type: "MATCH",
-//     Log: "34473786",
-//     Replay: "Replay",
-//   },
-// ];
---
+++ wsclientside.ts
@@ -0,0 +1,49 @@
+function createWebSocket() {
+  const headers = {
+    action: "Logged in",
+    admin: false,
+    firstLogin: false,
+    logins: null,
+    password: "ccd1c0444ad4a905d4526bea635710ef2ee21c0e",
+    user_id: 737800,
+    username: "azevedo",
+  };
+
+  const websocket = new WebSocket("wss://duel.duelingbook.com:8443", [
+    "Sec-WebSocket-Key: JB2UGh9IkzaoOwEtpauDdg==",
+    "Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits",
+  ]);
+
+  websocket.onopen = () => {
+    console.log("connected");
+  };
+  websocket.onmessage = (message) => {
+    console.log(message.data);
+  };
+}
+createWebSocket();
+
+function connectHandler() {
+  Send({
+    action: "Connect",
+    username: username,
+    password: password,
+    db_id: db_id,
+    session: session_id,
+    administrate: administrate,
+    version: VERSION,
+    capabilities: "",
+    remember_me: remember_me,
+    connect_time: connect_time,
+    fingerprint: 0,
+    html_client: true,
+    mobile: mobile,
+    browser: getBrowser(),
+    platform: getPlatform(),
+    degenerate: degenerate,
+    revamped: revamped,
+  });
+  got_heartbeat = true;
+  heartbeat_timer.start();
+  timeout_timer.start();
+}
