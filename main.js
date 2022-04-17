b6072f6a85783d43c9f50f2f7114cfaf858332a2
Pedro Azevedo <theazevedo@outlook.com>
2022-04-17 17:06:56 -0300

    add more 50 players on list

--- main.js
+++ main.js
@@ -17,36 +17,13 @@ try {
           lastUpdate: new Date().toISOString(),
         };
       })
-      .splice(0, 100);
+      .splice(0, 150);
 
   const top = await getTopFromDB().then(organizePlayers);
 
   console.time('request');
-  const newtop = [
-    ...top,
-    {
-      position: top.length + 1,
-      username: 'ReichYGO',
-      rating: '0',
-      wins: '0',
-      loses: '0',
-      draws: '0',
-      experience: '0',
-      lastUpdate: new Date().toISOString(),
-    },
-    {
-      position: top.length + 2,
-      username: 'Paulo "PRRJ" Goncalves',
-      rating: '0',
-      wins: '0',
-      loses: '0',
-      draws: '0',
-      experience: '0',
-      lastUpdate: new Date().toISOString(),
-    },
-  ];
   const result = await Promise.all(
-    newtop.map(async ({ username }, index) => {
+    top.map(async ({ username }, index) => {
       console.log(
         `--------------------${username.toUpperCase()}:${
           index + 1
@@ -95,7 +72,7 @@ try {
         }, [])
         .splice(0, 19);
 
-      return { ...newtop[index], matches: output };
+      return { ...top[index], matches: output };
     })
   );
   console.timeEnd('request');
